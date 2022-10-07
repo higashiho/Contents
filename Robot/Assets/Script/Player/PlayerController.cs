0c6ee3188caa94a4d08ad7f447f06cea01250baf
ryomayukumoto <ryomayukkun@gmail.com>
2022-10-07 16:59:13 +0900

    player挙動修正(3)

--- Robot/Assets/Script/Player/PlayerController.cs
+++ Robot/Assets/Script/Player/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     private GameObject player;
     [SerializeField]
     private float speed = 7.0f;
+    [SerializeField]
+    private float lowSpeed; //アイテムを持っている状態の自身のスピード
 
     private bool b_left;
     private bool b_right;
@@ -15,7 +17,8 @@ public class PlayerController : MonoBehaviour
     private float Bottom;
     private float Right;
     private float Left;
-    
+
+    public bool HaveItem = false;   //アイテムを持っているか
     void Start()
     {
         b_left = true;
@@ -29,7 +32,10 @@ public class PlayerController : MonoBehaviour
    
     void Update()
     {
-        move();
+        if (!HaveItem)
+            move();
+        else
+            goAway();
     }
 
     void move()
@@ -86,4 +92,59 @@ public class PlayerController : MonoBehaviour
       
 
     }
+    void goAway()
+    {
+        if (Input.GetKey("w"))
+        {
+
+            player.transform.rotation = new Quaternion(0, 0, 0, 0);
+            //player.transform.Rotate(new Vector3(0, 90, 0));
+            if (player.transform.position.z < Top)
+                player.transform.position += transform.forward * lowSpeed * Time.deltaTime;
+            b_left = true;
+            b_right = true;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            if (b_left)
+            {
+                player.transform.rotation = new Quaternion(0, 0, 0, 0);
+                player.transform.Rotate(new Vector3(0, -90, 0));
+                b_left = false;
+                b_right = true;
+            }
+            if (player.transform.position.x > Left)
+            {
+                player.transform.position += transform.forward * lowSpeed * Time.deltaTime;
+            }
+        }
+        if (Input.GetKey("s"))
+        {
+            player.transform.rotation = new Quaternion(0, 180, 0, 0);
+            if (player.transform.position.z > Bottom)
+            {
+                player.transform.position += transform.forward * lowSpeed * Time.deltaTime;
+            }
+            b_left = true;
+            b_right = true;
+        }
+        if (Input.GetKey("d"))
+        {
+            if (b_right)
+            {
+                player.transform.rotation = new Quaternion(0, 0, 0, 0);
+                player.transform.Rotate(new Vector3(0, 90, 0));
+                b_right = false;
+                b_left = true;
+            }
+            if (player.transform.position.x < Right)
+            {
+                player.transform.position += transform.forward * lowSpeed * Time.deltaTime;
+            }
+        }
+
+
+    }
 }
+
---
+++ Robot/Assets/Script/Player/Status_Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Status_Player : MonoBehaviour
+{
+    [SerializeField]
+    private int attack = 0;
+    [SerializeField]
+    private int defense = 0;
+    [SerializeField]
+    private int statusUp = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "AttackItem")
+        {
+            attack += statusUp;
+        }
+        if(other.gameObject.tag == "DefenseItem")
+        {
+            defense += statusUp;
+        }
+    }
+}
