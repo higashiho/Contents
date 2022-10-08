a51ffec3a1e5ed16a0e9d823decf1d548585a335
higashihokakeru <kakeru.0422.pojr@gmail.com>
2022-10-08 23:48:35 +0900

    BattleScene調整

--- Robot/Assets/Script/BattlScene/Player/Buttle_Col_Player.cs
+++ Robot/Assets/Script/BattlScene/Player/Buttle_Col_Player.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class Buttle_Col_Player : MonoBehaviour
 {
     [SerializeField]
-    private Battl_AttackEnemy battl_AttackEnemy;
+    private Battl_AttackEnemy battl_AttackEnemy;        //スクリプト格納用
+
+    [SerializeField]
+    private Battle_PlayerController battle_PlayerController;        //スクリプト格納用
+
+
+    private float Return = 1;   //壁に当たった時
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +25,19 @@ public class Buttle_Col_Player : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D col) 
     {
-
+        if (col.gameObject.tag == "LeftWall")
+        {
+            battle_PlayerController.Speed *= Return;
+        }
     }
+    private void OnCollisionExit2D(Collision2D col) 
+    {
+        if (col.gameObject.tag == "LeftWall")
+        {
+            battle_PlayerController.Speed *= Return;
+        }
+    }
+
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.tag == "AttackPoint")
